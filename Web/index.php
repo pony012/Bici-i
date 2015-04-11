8d87aa0cd23f95bc00a4feacc18b5107bfd8dafd
Alan Andrés Sánchez Castro <pony.alanandres@gmail.com>
2015-04-10 20:56:17 -0500

    Se añadió el header responsive al proyecto

--- Web/index.php
+++ Web/index.php
@@ -2,7 +2,8 @@
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
-	
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 	
 	<link href="bower_components/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
 	<!-- Include roboto.css to use the Roboto web font, material.css to include the theme and ripples.css to style the ripple effect -->
